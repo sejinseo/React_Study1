fe4e9aefdc416bdaec140f264501095023ae39eb
test <test@testui-MacBookPro.local>
2022-04-10 20:29:16 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+// application/X-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+// application/json
+app.use(bodyParser.json());
+// bodyParser: 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해주는 것
+
+const config = require('./config/key');
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sejin:abcd1234@mongodb.btidv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {res.send('just do it')})
+
+app.post('/register', (req, res) => {
+    // 회원가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
 
